3b84a9c8f0be7858571806f6a720ea0e5c5bf002
Amir <a313gputimal@gmail.com>
2024-05-01 18:09:44 +0330

    header

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -9,123 +9,89 @@ const Header = () => {
             setMenu('block')
         }
         else{
-            setMenu('hidden')            
+            setMenu('hidden')
         }
     }
 
     return ( 
         <div> 
             <div className="block sm:hidden">
-                <div className='flex pt-3 border-4 h-[70px]'>
-
-                    <div className="absolute border-2 right-[50px]  ">
-                        <img onClick={handleClick} className="w-[30px] h-[30px]" src="more.png"/>
+                <div className='flex pt-4 h-[70px]'>
+                <div onClick={handleClick} className="absolute right-[50px]">
+                        <img className="bg-blue-500 w-[30px] h-[30px] p-[3px] rounded-md" src="more.png"/>
                         <div className={menu}>
-                            <button className="  w-[170px] border-2 h-10 bg-blue-700 text-white rounded-full pb-[5px]"> ورود / ثبت نام </button>
-                            <NavLink to='/' className='w-[100px] block'> صفحه اصلی </NavLink>
-                            <NavLink to='/courses' className='block'> دوره </NavLink>
-                            <NavLink className='block'> مقالات </NavLink>
-                            <NavLink className='block'> اساتید </NavLink>
-                            <NavLink className='w-[100px] block'> ارتباط با ما </NavLink>
+                            <button className="w-[100px] h-[35px] border-b-2 bg-white hover:bg-gray-100"> ورود / ثبت نام </button>
+                            <NavLink to='/' className='w-[100px] block h-[35px] border-b-2 bg-white hover:bg-gray-100'> صفحه اصلی </NavLink>
+                            <NavLink to='/courses' className='block h-[35px] border-b-2 bg-white hover:bg-gray-100'> دوره </NavLink>
+                            <NavLink className='block h-[35px] border-b-2 bg-white hover:bg-gray-100'> مقالات </NavLink>
+                            <NavLink className='block h-[35px] border-b-2 bg-white hover:bg-gray-100'> اساتید </NavLink>
+                            <NavLink className='w-[100px] block h-[35px] border-b-2 bg-white hover:bg-gray-100'> ارتباط با ما </NavLink>
                         </div>
                     </div>
-
                     <h1 className="mr-[40%]"> BAHR ACADEMY </h1>
-
-
-
                 </div> 
             </div>
 
-
-
-
             <div className='hidden sm:block md:hidden'>
-                <div className='flex pt-3 border-4 h-[70px]'>
-
-                    <div className="absolute border-2 right-[50px]  ">
-                        <img onClick={handleClick} className="w-[30px] h-[30px]" src="more.png"/>
+                <div className='flex pt-4 h-[70px]'>
+                    <div onClick={handleClick} className="absolute right-[50px] bg-white">
+                        <img className="bg-blue-500 w-[30px] h-[30px] p-[3px] rounded-md" src="more.png"/>
                         <div className={menu}>
-                            <button className="  w-[170px] border-2 h-10 bg-blue-700 text-white rounded-full pb-[5px]"> ورود / ثبت نام </button>
-                            <NavLink to='/' className='w-[100px] block'> صفحه اصلی </NavLink>
-                            <NavLink to='/courses' className='block'> دوره </NavLink>
-                            <NavLink className='block'> مقالات </NavLink>
-                            <NavLink className='block'> اساتید </NavLink>
-                            <NavLink className='w-[100px] block'> ارتباط با ما </NavLink>
+                            <button className="w-[100px] h-[35px] border-b-2 bg-white hover:bg-gray-100"> ورود / ثبت نام </button>
+                            <NavLink to='/' className='w-[100px] block h-[35px] border-b-2 bg-white hover:bg-gray-100'> صفحه اصلی </NavLink>
+                            <NavLink to='/courses' className='block h-[35px] border-b-2 bg-white hover:bg-gray-100'> دوره </NavLink>
+                            <NavLink className='block h-[35px] border-b-2 bg-white hover:bg-gray-100'> مقالات </NavLink>
+                            <NavLink className='block h-[35px] border-b-2 bg-white hover:bg-gray-100'> اساتید </NavLink>
+                            <NavLink className='w-[100px] block h-[35px] border-b-2 bg-white hover:bg-gray-100'> ارتباط با ما </NavLink>
                         </div>
                     </div>
-
                     <h1 className="mr-[40%]"> BAHR ACADEMY </h1>
-
-
-
                 </div>  
             </div>
 
-
-
-
-            <div className='mx-auto hidden md:block lg:hidden'>
-                <div className='flex pt-3 border-4 h-[70px]'>
-
-                    <div className="absolute border-2 right-[50px]  ">
-                        <img onClick={handleClick} className="w-[30px] h-[30px]" src="more.png"/>
+            <div className='hidden md:block lg:hidden'>
+                <div className='flex pt-4 h-[70px]'>
+                    <div className="absolute right-[50px] bg-white">
+                        <img onClick={handleClick} className="bg-blue-500  w-[30px] h-[30px] p-[3px] rounded-md" src="more.png"/>
                         <div className={menu}>
-                            <NavLink to='/' className='w-[100px] block'> صفحه اصلی </NavLink>
-                            <NavLink to='/courses' className='block'> دوره </NavLink>
-                            <NavLink className='block'> مقالات </NavLink>
-                            <NavLink className='block'> اساتید </NavLink>
-                            <NavLink className='w-[100px] block'> ارتباط با ما </NavLink>
+                            <NavLink to='/' className='w-[90px] block border-b-2 bg-white hover:bg-gray-100 h-[35px]'> صفحه اصلی </NavLink>
+                            <NavLink to='/courses' className='block border-b-2 bg-white hover:bg-gray-100 h-[35px]'> دوره </NavLink>
+                            <NavLink className='block border-b-2 bg-white hover:bg-gray-100 h-[35px]'> مقالات </NavLink>
+                            <NavLink className='block border-b-2 bg-white hover:bg-gray-100 h-[35px]'> اساتید </NavLink>
+                            <NavLink className='w-[90px] block border-b-2 bg-white hover:bg-gray-100 h-[35px]'> ارتباط با ما </NavLink>
                         </div>
                     </div>
-
                     <h1 className="mr-[40%]"> BAHR ACADEMY </h1>
-
-
                     <button className="absolute left-[50px] w-[170px] border-2 h-10 bg-blue-700 text-white rounded-full pb-[5px]"> ورود / ثبت نام </button>
-                    
                 </div>  
             </div>
 
-
-
-
-        <div className='hidden lg:block xl:hidden'>
-                <div className='flex pt-3 border-4 h-[70px]'>
+            <div className='hidden lg:block xl:hidden'>
+                <div className='flex pt-3 h-[70px]'>
                     <h1 className="mr-[4%]"> BAHR ACADEMY </h1>
-
-                    <div className="flex gap-10 border-2 mr-[100px] ">
-                        <NavLink to='/' className='w-[100px]'> صفحه اصلی </NavLink>
+                    <div className="flex gap-[30px] mr-[50px] ">
+                        <NavLink to='/' className='w-[90px]'> صفحه اصلی </NavLink>
                         <NavLink to='/courses'> دوره </NavLink>
                         <NavLink> مقالات </NavLink>
                         <NavLink> اساتید </NavLink>
-                        <NavLink className='w-[100px]'> ارتباط با ما </NavLink>
+                        <NavLink className='w-[80px]'> ارتباط با ما </NavLink>
                     </div>
-
-                    <button className="absolute left-[50px] w-[170px] border-2 h-10 bg-blue-700 text-white rounded-full pb-[5px]"> ورود / ثبت نام </button>
-                    
+                    <button className="absolute left-[50px] w-[160px] border-2 h-10 bg-blue-700 text-white rounded-full pb-[5px]"> ورود / ثبت نام </button>
                 </div>  
-        </div>
-
-
-
-
+            </div>
 
             <div className='hidden xl:block'>
-                <div className='flex pt-3 border-4 h-[70px]'>
+                <div className='flex pt-3 h-[70px]'>
                     <h1 className="mr-[7%]"> BAHR ACADEMY </h1>
-
-                    <div className="flex gap-10 border-2 mr-[100px] ">
-                        <NavLink to='/' className='w-[100px]'> صفحه اصلی </NavLink>
+                    <div className="flex gap-[30px] mr-[50px] ">
+                        <NavLink to='/' className='w-[90px]'> صفحه اصلی </NavLink>
                         <NavLink to='/courses'> دوره </NavLink>
                         <NavLink> مقالات </NavLink>
                         <NavLink> اساتید </NavLink>
-                        <NavLink className='w-[100px]'> ارتباط با ما </NavLink>
+                        <NavLink className='w-[80px]'> ارتباط با ما </NavLink>
                     </div>
-
-                    <button className="absolute left-[100px] w-[200px] border-2 h-10 bg-blue-700 text-white rounded-full pb-[5px]"> ورود / ثبت نام </button>
-                    
-                </div>               
+                    <button className="absolute left-[100px] w-[150px] border-2 h-10 bg-blue-700 text-white rounded-full pb-[5px]"> ورود / ثبت نام </button>
+                </div>    
             </div>
 
          </div>
--- src/rout/leyout.jsx
+++ src/rout/leyout.jsx
@@ -6,7 +6,7 @@ const Leyout = () => {
     return ( 
         <>
             <Header />
-            <Outlet />
+            {/* <Outlet /> */}
             <Footer />
         </>
      );
