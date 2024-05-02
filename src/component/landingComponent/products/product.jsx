145c10e7bbce2776aec160375ccbae59a839bcd7
Amir <a313gputimal@gmail.com>
2024-05-02 16:15:17 +0330

    dfg

--- src/component/landingComponent/products/product.jsx
+++ src/component/landingComponent/products/product.jsx
@@ -2,7 +2,7 @@ import Rectangle from "../../../../public/Rectangle.png";
 
 const Product = () => {
     return ( 
-        <div>
+        <div className='hidden xl:block'>
             <img className="w-[500px] h-[500px] absolute top-0" src={Rectangle}/>
             <h1> آکادمی بحر ... </h1>
         </div>
