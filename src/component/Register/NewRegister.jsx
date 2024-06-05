b2d910ce9ead8468d989774f819655b69d66fa50
amir mohamad <a313gputimal@gmail.com>
2024-06-05 16:20:07 +0330

    asdw

--- src/component/Register/NewRegister.jsx
+++ src/component/Register/NewRegister.jsx
@@ -3,52 +3,13 @@ import FirstStep from "./FirstStep/FirstStep";
 import SecondStep from "./SecondStep/SecondStep";
 import ThirdStep from "./ThirdStep/ThirdStep";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-=======
 import register from "../../../public/mobile-testing.png";
->>>>>>> Amir/API
 
 const NewRegister = () => {
   const windowHeight = window.innerHeight;
   const [StepCounter, setStepCounter] = useState(1);
   return (
     <>
-<<<<<<< HEAD
-      <div
-        className="bg-contain bg-opacity-75 relative "
-        style={{ height: windowHeight }}
-      >
-        <div
-          className=" bg-forgetBackGround bg-contain blur-md"
-          style={{ height: windowHeight }}
-        ></div>
-        <div className="w-[85%] h-[520px] bg-blue-400/50 rounded-[10px] flex absolute top-[100px] left-[115px] ">
-          <div className="l bg-blue-400 w-[65%] h-[100%] rounded-xl">
-            <div className="lheader  flex w-[95%] h-[8%] mx-auto mt-[10px] ">
-              <Link
-                to="/login"
-                className="lheaderr h-[100%] w-[50%] flex indent-3 text-white leading-[45px] font-normal justify-start "
-              >
-                <img
-                  className="w-[25px] h-[25px] mt-[10px]"
-                  src="../../public/icons8-enter-32.png"
-                />
-                ورود به سایت
-              </Link>
-              <div className="lheaderl h-[100%] w-[50%] flex  gap-[20px] justify-end">
-                <Link className=" mt-[10px]">
-                  {" "}
-                  <img
-                    className="h-[35px] w-[35px]"
-                    src="../../public/icons8-sun-48.png"
-                  />{" "}
-                </Link>
-                <Link to="/" className=" mt-[9px]">
-                  {" "}
-                  <img src="../../public/icons8-home-32.png" />{" "}
-                </Link>
-              </div>
-=======
 
       <div className='block sm:hidden'>
       <div
@@ -89,7 +50,6 @@ const NewRegister = () => {
               {StepCounter === 3 && (
                 <ThirdStep setStepCounter={setStepCounter} />
               )}
->>>>>>> Amir/API
             </div>
 
             {StepCounter === 1 && <FirstStep setStepCounter={setStepCounter} />}
@@ -101,13 +61,6 @@ const NewRegister = () => {
         </div>
       </div>
 
-<<<<<<< HEAD
-      <div className="bg-forgetBlur"></div>
-    </>
-  );
-};
-
-=======
       <div className='hidden sm:block md:hidden'>
       <div
           className="bg-contain bg-opacity-75 relative flex justify-center"
@@ -291,5 +244,4 @@ const NewRegister = () => {
   );
 };
 
->>>>>>> Amir/API
 export default NewRegister;
